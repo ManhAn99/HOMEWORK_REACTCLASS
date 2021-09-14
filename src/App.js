89c52bd3a1c729304fb7390e4aa344504560fc07
Manh An <an49248@gmail.com>
2021-09-14 14:39:55 +0700

    add homework10

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import axios from 'axios'
 import {useEffect,useState} from 'react'
 import Header from './components/Header'
 import Products from './components/Products'
-
+import CircularProgress from '@material-ui/core/CircularProgress';
   function App() {
-
+  const [isLoading, setIsLoading] = useState(true)
   const [products,setProducts] = useState([])
   const [searchProducts,setSearchProducts] = useState([])
+  const [sort,setSort] = useState('latest')
+  const [category,setCategory] = useState('all')
  
   useEffect(() => {
       axios.get('https://fakestoreapi.com/products')
@@ -17,6 +19,14 @@ import Products from './components/Products'
         .catch(err => console.log(err))
   }, [])
 
+  useEffect(() => {
+    const timeOut  = setTimeout(() => {
+      setIsLoading(false)
+    },3000)
+    
+    return () => clearTimeout(timeOut)
+  }, [])
+  //search
   const handleOnChange = (value) => {
     
     if(value === '') {
@@ -30,12 +40,45 @@ import Products from './components/Products'
     
     console.log(value);
   }
-  console.log(searchProducts);
+  //sort price
+  const handleSort = (value) => {
+    setSort(value)
+    setSearchProducts(products.sort((a,b) => (
+      value === 'lowest'
+      ? a.price - b.price
+      : value === 'highest'
+      ? b.price - a.price
+      : a.id - b.id
+    )))
+  }
+  //sort category
+  const handleCategory = (value) => {
+    setCategory(value)
+    if(value === 'all') {
+      setSearchProducts(products)
+    }
+    else{
+      setSearchProducts(products.filter(product => (
+        product.category === value
+      )))
+    }
+  }
+
   return (
     <div>
-       <Header handleOnChange = {handleOnChange} />
-       <Products products = {searchProducts} />
-     
+       <Header 
+       handleCategory = {handleCategory}
+       category = {category}
+       sort = {sort}
+       handleOnChange = {handleOnChange} 
+       handleSort= {handleSort} />
+       {isLoading ? (
+         <div className = 'flex justify-center mt-12'>
+          <CircularProgress/> 
+         </div>
+        )
+       : <Products products = {searchProducts} /> 
+       }
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Image from '../assets/commerce.png'
+import Image from '../assets/logo.jpg'
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import { Badge } from '@material-ui/core';
+import { Badge,Select,MenuItem } from '@material-ui/core';
 import { TextField } from '@material-ui/core';
-const Header = ({handleOnChange}) => {
+const Header = ({handleOnChange,handleSort,sort,category,handleCategory}) => {
     return (
         <div className = 'p-5 shadow-md h-20 flex items-center justify-between'>
             <div className = 'flex items-center'>
@@ -12,15 +12,49 @@ const Header = ({handleOnChange}) => {
                 </div>
                 <h1 className = 'pl-3 text-blue-900 font-medium text-xl'>E-Commerce</h1>
             </div>
-            <TextField 
-             id="standard-basic" 
-             label="Search"
-             onChange = {(e) => handleOnChange(e.target.value)}
-             />
-            <Badge className = 'cursor-pointer' badgeContent={4} color="secondary">
-              <ShoppingCartIcon />
-             </Badge>
-        </div>
+            <div className = 'flex items-center'>
+              
+              <TextField 
+               
+               id="standard-basic" 
+               label="Search"
+               onChange = {(e) => handleOnChange(e.target.value)}
+                 />
+               
+
+               <div className = 'flex items-center pt-4'>
+               <Select
+                 labelId="demo-simple-select-label"
+                 id="demo-simple-select"
+                 value={sort}
+                 onChange = {(e) => handleSort(e.target.value)}
+                 className = 'mx-5'
+                   >
+               <MenuItem value= 'latest'>Latest</MenuItem>
+               <MenuItem value= 'highest'>Highest</MenuItem>
+                <MenuItem value= 'lowest'>Lowest</MenuItem>
+               </Select>
+
+               <Select
+                 className = 'mx-5'
+                 labelId="demo-simple-select-label"
+                 id="demo-simple-select"
+                 value={category}
+                 onChange = {(e) => handleCategory(e.target.value)}
+                   >
+               <MenuItem value= 'all'>All</MenuItem>
+               <MenuItem value= "men's clothing">Men</MenuItem>
+                <MenuItem value= "women's clothing">Woman</MenuItem>
+                <MenuItem value= 'jewelery'>Jewelery</MenuItem>
+                <MenuItem value= 'electronics'>Electronics</MenuItem>
+               </Select>
+              
+               <Badge className = 'cursor-pointer' badgeContent={4} color="secondary">
+                <ShoppingCartIcon />
+               </Badge>
+               </div>
+             </div>
+             </div>
     )
 }
 
